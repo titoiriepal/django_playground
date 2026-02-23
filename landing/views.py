b6d3a7f78adc242ef56593aa0af85bc5c8cb8804
titoiriepal <titoiriepal@hotmail.com>
2026-02-23 17:11:25 +0100

    context, filters and if tags into landing.html

---
+++ landing/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from datetime import date
+# Create your views here.
+
+
+def home(request):
+    return render(
+        request,
+        "landing/landing.html",
+        context={
+            "name": "tito",
+            "age": 30,
+            "today": date.today(),
+        },
+    )
