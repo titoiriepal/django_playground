1da557f3ee6b4008227f1febd4fcb207177a2e47
titoiriepal <titoiriepal@hotmail.com>
2026-02-23 16:43:42 +0100

    app landing create with templates

--- playground/urls.py
+++ playground/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('messages/', include('quotes.urls')),
+    path('landings/', include('landing.urls')),
 ]
