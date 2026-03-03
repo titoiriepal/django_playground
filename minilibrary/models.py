d3104c09bcbda1afb0a3a56fba1f0dc204d48b3e
titoiriepal <titoiriepal@hotmail.com>
2026-03-03 12:57:59 +0100

    Models Author and Book created

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='books'
+    )
+    publication_date = models.DateField(null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    isbn = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
