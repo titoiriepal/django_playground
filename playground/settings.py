74f4a37a14c36a4177717f2ed2470aa3e4e2f9fa
titoiriepal <titoiriepal@hotmail.com>
2026-03-05 10:30:04 +0100

    .gitignore added to the project. envairoment variables added to the project

--- playground/settings.py
+++ playground/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh-56xsa@hrvmo#4m-g#scy^@!1#o6r(ck&-epo6hcgkb=ei#%'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
