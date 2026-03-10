ba69d82285d6394ffdef6e95590b07f8eb2e9368
titoiriepal <titoiriepal@hotmail.com>
2026-03-10 09:48:56 +0100

    minilibrary/admin empty

--- minilibrary/admin.py
+++ minilibrary/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookDetail, Review, Recommendation, Loan
 
 # Register your models here.
-
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookDetail)
-admin.site.register(Review)
-admin.site.register(Recommendation)
-admin.site.register(Loan)
